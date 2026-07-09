d0c0d91c306e41b84e3c89ceb6a15f8f26ae2982
cybercat39-droid <cybercat39@gmail.com>
2026-07-09 07:49:15 +0700

    Update qbittorrent_th.ts

--- src/lang/qbittorrent_th.ts
+++ src/lang/qbittorrent_th.ts
@@ -700,7 +700,7 @@
         <location filename="../gui/advancedsettings.cpp" line="564"/>
         <location filename="../gui/advancedsettings.cpp" line="704"/>
         <source> MiB</source>
-        <translation>เมบิไบต์</translation>
+        <translation>เมกะไบต์</translation>
     </message>
     <message>
         <location filename="../gui/advancedsettings.cpp" line="776"/>
@@ -13375,37 +13375,37 @@ Please choose a different name and try again.</source>
         <location filename="../base/utils/misc.cpp" line="60"/>
         <source>KiB</source>
         <comment>kibibytes (1024 bytes)</comment>
-        <translation>กิบิไบต์</translation>
+        <translation>กิโลไบต์</translation>
     </message>
     <message>
         <location filename="../base/utils/misc.cpp" line="61"/>
         <source>MiB</source>
         <comment>mebibytes (1024 kibibytes)</comment>
-        <translation>เมบิไบต์</translation>
+        <translation>เมกะไบต์</translation>
     </message>
     <message>
         <location filename="../base/utils/misc.cpp" line="62"/>
         <source>GiB</source>
         <comment>gibibytes (1024 mibibytes)</comment>
-        <translation>จิบิไบต์</translation>
+        <translation>กิกะไบต์</translation>
     </message>
     <message>
         <location filename="../base/utils/misc.cpp" line="63"/>
         <source>TiB</source>
         <comment>tebibytes (1024 gibibytes)</comment>
-        <translation>เทบิไบต์</translation>
+        <translation>เทราไบต์</translation>
     </message>
     <message>
         <location filename="../base/utils/misc.cpp" line="64"/>
         <source>PiB</source>
         <comment>pebibytes (1024 tebibytes)</comment>
-        <translation>เพบิไบต์</translation>
+        <translation>เพตะไบต์</translation>
     </message>
     <message>
         <location filename="../base/utils/misc.cpp" line="65"/>
         <source>EiB</source>
         <comment>exbibytes (1024 pebibytes)</comment>
-        <translation>เอกซ์บิไบต์</translation>
+        <translation>เอกซาไบต์</translation>
     </message>
     <message>
         <location filename="../base/utils/misc.cpp" line="101"/>
@@ -13423,7 +13423,7 @@ Please choose a different name and try again.</source>
         <location filename="../base/utils/misc.cpp" line="244"/>
         <source>%1m</source>
         <comment>e.g: 10 minutes</comment>
-        <translation>% 1 นาที</translation>
+        <translation>%1 นาที</translation>
     </message>
     <message>
         <location filename="../base/utils/misc.cpp" line="250"/>
